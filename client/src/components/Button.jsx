33e3f9a43635a6d3915344977c7c3fc02018f875
tanya17-05 <tanyakumari332@gmail.com>
2022-10-15 13:52:10 +0530

    update: UI

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,9 +1,9 @@
-const Button = ({type}) => {
+const Button = ({type, click}) => {
 
   let buttonName = (type === `reset`) ? `Reset` : `Check Mail`;
-
+  console.log(click)
   return (
-      <button type={type} className={type}>{buttonName}</button>
+      <button type={type} className={type} onClick={click}>{buttonName}</button>
   )
 }
 
